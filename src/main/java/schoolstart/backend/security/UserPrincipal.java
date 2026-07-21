e3b7f16242c799a1ab56e3fa362705cb583878fd
Asiri <Asirimadhuranga2022@gmail>
2026-07-21 15:22:24 +0530

    Add UserPrincipal for Spring Security authentication

---
+++ src/main/java/schoolstart/backend/security/UserPrincipal.java
@@ -0,0 +1,86 @@
+package schoolstart.backend.security;
+
+import schoolstart.backend.entity.UserModel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
+
+@AllArgsConstructor
+public class UserPrincipal implements UserDetails {
+    @Getter
+    private String id;
+
+    private String username;
+
+    @Getter
+    private String email;
+
+    private String password;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public static UserPrincipal create(UserModel user) {
+        GrantedAuthority authority = new SimpleGrantedAuthority(user.getRole().name());
+        return new UserPrincipal(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPassword(),
+                Collections.singletonList(authority)
+        );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserPrincipal that = (UserPrincipal) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
